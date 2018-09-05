df6590d36b354bc4b78e0c2ec3f04298ebf22d01
Bas Kager <baskager@hotmail.com>
2018-09-05 16:45:31 +0200

    Components: Add comments to index.js

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,9 @@
+/**
+ * Function that loads custom libraries
+ *
+ * @since: 12-08-2018
+ * @author: Bas Kager
+ */
 module.exports = function(config) {
   const Cache = require("./cache/Cache.class")(config.cache);
   let cache = new Cache();
