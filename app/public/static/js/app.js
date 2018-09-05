3d56d65359ebf2cc7c96476c705e0bacf2eabc5a
Bas Kager <baskager@hotmail.com>
2018-09-05 12:18:44 +0200

    Serviceworker: Update image locations before improving the service worker

--- app/public/static/js/app.js
+++ app/public/static/js/app.js
@@ -1,15 +1,1 @@
-// Feature detection for the ServiceWorker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(function(reg) {
-      // registration worked
-      console.log(
-        "ServiceWorker registration succeeded. Scope is " + reg.scope
-      );
-    })
-    .catch(function(error) {
-      // registration failed
-      console.log("ServiceWorker registration failed with " + error);
-    });
-}
+"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js").then(function(e){console.log("ServiceWorker registration succeeded. Scope is "+e.scope)}).catch(function(e){console.log("ServiceWorker registration failed with "+e)});
